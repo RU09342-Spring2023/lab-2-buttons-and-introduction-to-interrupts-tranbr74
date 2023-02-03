33c53a68c92d36b40a159b65fb2c9a6d70bff167
tranbr74 <123009043+tranbr74@users.noreply.github.com>
2023-02-02 23:25:56 -0500

    Add files via upload

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -1,9 +1,9 @@
 /*
  *  Pull-Up Resistor Configuration
  *
- *  Created on: Jan 31, 2023
- *      Author: Russell Trafford
- *      Version: 1.0
+ *  Created on: Jan 30, 2023
+ *      Author: Brandon Tran
+ *      Version: 2.0
  *
  *      This example will show you how to configure the Pull-up Resistor for your button inputs.
  *
@@ -23,6 +23,7 @@ int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
+
     gpioInit();                 // Initialize all GPIO Pins for the project
 
 
@@ -32,9 +33,9 @@ int main(void)
 
     while(1)
     {
-        if (P2IN & BIT3)            // If S2 (P2.3) is pressed
+        if (!(P2IN & BIT3))            // If S2 (P2.3) is pressed
             P6OUT ^= BIT6;          // Toggle P6.6
-        if (P4IN & BIT1)            // If S1 (P4.1) is pressed
+        if (!(P4IN & BIT1))            // If S1 (P4.1) is pressed
             P1OUT ^= BIT0;          // Toggle P1.0
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
     }
